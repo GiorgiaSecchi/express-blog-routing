0b48e2bad6f2736345518892978c54327b316fe0
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-20 16:05:07 +0100

    add db posts

---
+++ db/posts-db.js
@@ -0,0 +1,39 @@
+//# ARRAY 5 POST
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "La ricetta",
+    immagine: "/img/ciambellone.jpeg",
+    tags: ["cucina", "ricetta", "ciambellone"],
+  },
+
+  {
+    titolo: "Cracker di barbabietola",
+    contenuto: "La ricetta",
+    immagine: "/img/cracker_barbabietola.jpeg",
+    tags: ["cucina", "ricetta", "barbabietola"],
+  },
+
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "La ricetta",
+    immagine: "/img/pane_fritto_dolce.jpeg",
+    tags: ["cucina", "ricetta", "pane"],
+  },
+
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "La ricetta",
+    immagine: "/img/pasta_barbabietola.jpeg",
+    tags: ["cucina", "ricetta", "pasta"],
+  },
+
+  {
+    titolo: "Torta paesana",
+    contenuto: "La ricetta",
+    immagine: "/img/torta_paesana.jpeg",
+    tags: ["cucina", "ricetta", "torta"],
+  },
+];
+
+module.exports = posts;
--- routers/posts.js
+++ routers/posts.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const posts = require("db/posts-db.js");
+
 //# ROTTE POSTS
 
 //* index
+app.get();
 
 module.exports = router;
